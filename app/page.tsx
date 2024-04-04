f12b45503b5532a3088ed2d1172d2ebf2792d487
romingoon <romingoon@gmail.com>
2024-04-04 12:32:51 +0900

    Naver 지도 메인페이지에 띄우기

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import ConnectSupabaseSteps from '@/components/tutorial/ConnectSupabaseSteps';
 import SignUpUserSteps from '@/components/tutorial/SignUpUserSteps';
 import Header from '@/components/Header';
 import { cookies } from 'next/headers';
+import { useRef } from 'react';
+import NaverMap from '@/components/NaverMap';
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -22,7 +24,6 @@ export default async function Index() {
   };
 
   const isSupabaseConnected = canInitSupabaseClient();
-
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
@@ -35,6 +36,7 @@ export default async function Index() {
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
         <main className="flex-1 flex flex-col gap-6">
+          <NaverMap />
           <h2 className="font-bold text-4xl mb-4">Next steps</h2>
           {isSupabaseConnected ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
         </main>
