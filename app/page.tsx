1d4db11e300a6cff2d332b4d819e47fc3944af35
romingoon <romingoon@gmail.com>
2024-04-08 17:32:46 +0900

    Main page 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,6 @@
-import DeployButton from '../components/DeployButton';
-import AuthButton from '../components/AuthButton';
-import { createClient } from '@/utils/supabase/server';
-import ConnectSupabaseSteps from '@/components/tutorial/ConnectSupabaseSteps';
-import SignUpUserSteps from '@/components/tutorial/SignUpUserSteps';
-import Header from '@/components/Header';
-import { cookies } from 'next/headers';
-import { useRef } from 'react';
 import MapBox from '@/components/MapBox';
 
-export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to rem  const cookieStore = cookies();
-    const cookieStore = cookies();
-    const supabase = createClient();
-
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
+export default function Index() {
   return (
     <div>
       <main>
